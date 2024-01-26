ebc34dcb197b746fea3996559dd7bf059d070fa1
shayrma <148952261+shayrmaq@users.noreply.github.com>
2024-01-26 19:06:53 +1000

    Add files via upload

---
+++ Square.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Square : MonoBehaviour
+{
+    public Vector2 targetPosition;
+    public float moveStep;
+    public bool isTrap;
+    public float speedFactor;
+    public float scaleFactor;
+    public int catchCount;
+    Vector2 GetRandomPoint()
+    {
+        Vector2 randomVector = new Vector2();
+            randomVector.x = Random.Range(-6,6);
+        randomVector.y = Random.Range(-3,3);
+        return randomVector;
+    }
+    void OnMouseDown()
+    {
+        if (isTrap)
+        {
+            Player.Defeat();
+        }
+        else
+        {
+            Catch();
+        }
+    }
+    void Catch()
+    {
+        Player.score++;
+        catchCount--;
+        if (catchCount == 0)
+        {
+            Player.squares.Remove(this);
+            Destroy(gameObject);
+        }
+        else
+        {
+            moveStep += speedFactor;
+            transform.localScale = (Vector2)transform.localScale - new Vector2(scaleFactor, scaleFactor);
+            transform.position = GetRandomPoint();
+
+        }
+    }
+    void Start()
+    {
+        if (isTrap == false)
+        {
+            Player.squares.Add(this);
+        }
+        targetPosition = GetRandomPoint();
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+            targetPosition, moveStep * Time.deltaTime);
+        if ((Vector2)transform.position == targetPosition)
+        {
+            targetPosition = GetRandomPoint();
+        }
+
+    }
+}
